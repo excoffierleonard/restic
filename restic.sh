7afddd60bc1a9e3c9aef67fa1f08d255401e75c8
Leonard Excoffier <48970393+excoffierleonard@users.noreply.github.com>
2026-08-29 22:22:19 -0400

    enhance backup command to exclude files based on .resticignore

--- restic.sh
+++ restic.sh
@@ -19,6 +19,6 @@ set +a
 # Perform backup and prune
 echo "=== Backup started: $(date) ==="
 restic cat config >/dev/null 2>&1 || { [ $? -eq 10 ] && restic init; }
-restic backup /app/backup -v 
+restic backup /app/backup --exclude-if-present .resticignore -v
 restic forget -d 7 -w 4 -m 12 -y 100 --prune -v
 echo "=== Backup finished: $(date) ==="
